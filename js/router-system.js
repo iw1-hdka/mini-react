1aca615ea909bd8c50e06bcfcfe678a8fdd04638
Kaba CONDE <kabaconde15@gmail.com>
2019-02-25 18:33:07 +0100

    Add router-system avoid conflict

---
+++ js/router-system.js
@@ -0,0 +1,23 @@
+"use strict";
+
+import { createElement as h, render, Component } from "./component.js";
+import Router from "./router.js";
+import { routes } from "./routes.js";
+
+var router = new Router(routes)
+class App extends Component {
+  render() {
+    return h(router.load())
+  }
+}
+
+// App entry point
+let app = () => {
+  render(App, document.getElementById("root"));
+}
+
+// Listen on hash change:
+window.addEventListener("hashchange", app);
+
+// Listen on page load:
+window.addEventListener("load", app);
